860b14b926359b8fdd2d5b006e57cf29d78c777f
Kalindhu_Navanjana <kalindhunavanjana@gmail.com>
2021-05-05 02:23:48 +0530

    created user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,23 @@
-import React, { Component } from "react";
-import Spinner from "../layout/Spinner"
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    };
+
     render() {
         const {
             name,
+            company,
             avatar_url,
             location,
             bio,
@@ -23,7 +33,69 @@ class User extends Component {
 
         const { loading } = this.props;
 
-        return <div>{name}</div>;
+        if (loading) {
+            return <Spinner />;
+        }
+
+        return (
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>
+                Hireable:{" "}
+                {hireable ? (
+                    <i className='fas fa-check text-success'></i>
+                ) : (
+                    <i className='fas fa-times-circle text-danger'></i>
+                )}
+                <div className='card grid-2'>
+                    <div className='all-center'>
+                        <img
+                            src={avatar_url}
+                            className='round-img'
+                            alt=''
+                            style={{ width: "150px" }}
+                        />
+                        <h1>{name}</h1>
+                        <p>Location : {location} </p>
+                    </div>
+                    <div>
+                        {bio && (
+                            <Fragment>
+                                <h3>Bio</h3>
+                                <p>{bio}</p>
+                            </Fragment>
+                        )}
+                        <a href={html_url} className='btn btn-dark my-1'>
+                            Visit GirHub Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Username :</strong> {login}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {company && (
+                                    <Fragment>
+                                        <strong>Company :</strong> {company}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {blog && (
+                                    <Fragment>
+                                        <strong>Website :</strong> {blog}
+                                    </Fragment>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </Fragment>
+        );
     }
 }
 
